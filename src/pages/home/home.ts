46e97ee503dea66cce77592096aaa78572fb7f75
Jon Lauters <jonathan.lauters@gmail.com>
2016-12-08 11:13:54 -0600

    added map lading msg

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, Input,  ViewChild, ElementRef } from '@angular/core';
-import { NavController, ModalController, Platform, NavParams, ViewController } from 'ionic-angular';
+import { NavController, ModalController, Platform, NavParams, ViewController, LoadingController } from 'ionic-angular';
 import { Geolocation } from 'ionic-native';
+import { Monster } from './monsters';
 import { ModalContentPage } from './modal-detail.component';
 import { ModalBugBox } from './modal-bugbox.component';
 
@@ -17,7 +18,7 @@ export class HomePage {
   map: any;
   monsters: any;
 
-  constructor(public modalCtrl: ModalController) { }
+  constructor(public modalCtrl: ModalController, public loadingCtrl: LoadingController) { }
 
   ionViewDidLoad() {
     this.loadMap();
@@ -34,17 +35,30 @@ export class HomePage {
     }
   }
 
+  presentLoading() {
+    let loader = this.loadingCtrl.create({
+      content: "Loading Map ...",
+      duration: 3000
+    });
+
+    loader.present();
+  }
+
   loadMap() {
   
+    // loading msg
+    this.presentLoading();
+
     // sample test data
-    let monsters = [
+    let monsters: Monster[] = [
       {
         id: 1,
         name: 'Chironomid',
         icon: 'http://phylopic.org/assets/images/submissions/834f9ef5-c5bf-4e9e-94c8-3ecb8fb14838.64.png',
         difficulty: 1,
         lat: 41.654085,
-        lng: -91.525087
+        lng: -91.525087,
+        found: false
       },
       {
         id: 2,
@@ -52,7 +66,8 @@ export class HomePage {
         icon: 'http://phylopic.org/assets/images/submissions/e3fde32c-34c2-4f67-8577-e546909f83e8.64.png',
         difficulty: 3,
         lat: 41.654145,
-        lng: -91.522978
+        lng: -91.522978,
+        found: false
       },
       {
         id: 3,
@@ -60,7 +75,8 @@ export class HomePage {
         icon: 'http://phylopic.org/assets/images/submissions/becb252d-fd39-4124-97c8-208aa5d448db.64.png',
         difficulty: 2,
         lat: 41.654100,
-        lng: -91.523265
+        lng: -91.523265,
+        found: false
       }
     ];
 
